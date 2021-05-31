370454614e834087ba486dfb7a678965fdf66734
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-05-31 22:20:11 +0200

    Add files via upload

---
+++ MSWORD-REPLACE.py
@@ -0,0 +1,29 @@
+# ADGSTUDIOS 2021
+# WORD-GENERATOR
+from docx import Document
+
+
+def ProcessDocument(template_file_path, output_file_path, dictionary):
+    variables = dictionary
+    template_document = Document(template_file_path)
+
+    for variable_key, variable_value in variables.items():
+        for paragraph in template_document.paragraphs:
+            replace_text_in_paragraph(paragraph, variable_key, variable_value)
+
+        for table in template_document.tables:
+            for col in table.columns:
+                for cell in col.cells:
+                    for paragraph in cell.paragraphs:
+                        replace_text_in_paragraph(
+                            paragraph, variable_key, variable_value)
+
+    template_document.save(output_file_path)
+
+
+def replace_text_in_paragraph(paragraph, key, value):
+    if key in paragraph.text:
+        inline = paragraph.runs
+        for item in inline:
+            if key in item.text:
+                item.text = item.text.replace(key, value)
