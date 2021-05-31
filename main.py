b0a751390832e959f5f12b1982a9a53b19175496
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2021-05-31 21:31:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from docx import Document
+import os
+def main():
+    template_file_path = 'test.docx'
+    output_file_path = 'result.docx'
+
+    variables = {
+       "${yourvarname}: "what_ever_data_you_want"
+    }
+
+    template_document = Document(template_file_path)
+
+    for variable_key, variable_value in variables.items():
+        for paragraph in template_document.paragraphs:
+            replace_text_in_paragraph(paragraph, variable_key, variable_value)
+
+        for table in template_document.tables:
+            for col in table.columns:
+                for cell in col.cells:
+                    for paragraph in cell.paragraphs:
+                        replace_text_in_paragraph(paragraph, variable_key, variable_value)
+
+    template_document.save(output_file_path)
+
+def replace_text_in_paragraph(paragraph, key, value):
+    if key in paragraph.text:
+        inline = paragraph.runs
+        for item in inline:
+            if key in item.text:
+                item.text = item.text.replace(key, value)
+
+if __name__ == '__main__':
+    main()
